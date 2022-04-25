78ade5c287ad1b52c3b1357e37c381c52b518c61
ravula7 <ravula7@ad.unc.edu>
2022-04-25 14:09:05 -0400

    database

--- database.js
+++ database.js
@@ -7,7 +7,8 @@ let row = statement.get();
 if(row === undefined){
     console.log('Log database appears to be empty. Creating log database...')
     const sqlInit = `
-        CREATE TABLE accesslog ( remote-addr VARCHAR, remote-user VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, http-version NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
+        CREATE TABLE accesslog ( remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
+        INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES ('req.ip', 'req.user', Date.now(), 'req.method', 'req.url', 'req.protocol', 'req.httpVersion', 'res.statusCode', 'req.headers['referer'], 'req.headers['user-agent'])
     `;
         logdb.exec(sqlInit)
         console.log('Your database has now been initialized')
