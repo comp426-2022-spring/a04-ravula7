c559d44a005bd10c11d7aea3bb9f98e04dcfc165
ravula7 <ravula7@ad.unc.edu>
2022-04-25 17:17:22 -0400

    db

--- log.js
+++ log.js
@@ -8,7 +8,7 @@ let row = statement.get();
 if(row === undefined){
     console.log('Initializing database')
     const sqlInit = `
-        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
+        CREATE TABLE accesslog ( remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
     `;
         logdb.exec(sqlInit)
         console.log('Your database has now been initialized')
